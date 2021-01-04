444dbeafbccadb1604a9cc17c88b78f5cc255d21
Illoydius <illoydius@gmail.com>
2021-01-04 17:39:05 -0500

    initial

---
+++ Client/UI/DaisyControl/Utils/ImageUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using DaisyControl.Client.DaisyConnect.Managers;
+using DaisyControl.Common.DaisyUtils;
+
+namespace DaisyControl.Client.DaisyConnect.Utils
+{
+    internal static class ImageUtils
+    {
+        // ********************************************************************
+        //                            Internal
+        // ********************************************************************
+        /// <summary>
+        /// Load an image from disk with validations and convert it to BitmapImage
+        /// </summary>
+        /// <param name="aImagePath"></param>
+        /// <returns></returns>
+        internal static BitmapImage GetBitmapImageFromImageOnDisk(string aImagePath)
+        {
+            if (!FileUtils.CheckIfFileExistsOnDisk(aImagePath))
+                ErrorManager.RegisterNewError("DE5B4646-373D-49FA-8A8E-1A91094A3FCC", $"File [{aImagePath}] wasn't found.", aExitApplicationAfterMessage: true);
+
+            BitmapImage _Image = null;
+
+            try
+            {
+                _Image = new BitmapImage(new Uri($"{Environment.CurrentDirectory}\\{aImagePath}"));
+            } catch (Exception _Ex)
+            {
+                ErrorManager.RegisterNewError("CBD30AE6-40EA-4759-AF46-69AC89D40813", $"Image [{aImagePath}] is invalid or corrupted.", _Ex, aExitApplicationAfterMessage: true);
+            }
+
+            return _Image;
+        }
+    }
+}
---
+++ Client/UI/DaisyControl/View/Chat/ChatMessageControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DaisyControl.Client.DaisyConnect.View.Chat
+{
+    /// <summary>
+    /// Interaction logic for ChatMessageControl.xaml
+    /// </summary>
+    public partial class ChatMessageControl : UserControl
+    {
+        public ChatMessageControl()
+        {
+            InitializeComponent();
+        }
+
+        public ChatMessageControl(ChatMessage aChatMessage) : this()
+        {
+            if(aChatMessage.IsAIMessage)
+                Grid.SetColumn(MessageStackPanel, 2);
+
+            Border.CornerRadius = new CornerRadius(aChatMessage.ChatMessagePreference.BorderRadius);
+            Border.BorderThickness = new Thickness(aChatMessage.ChatMessagePreference.BorderSize);
+            Border.Background = new SolidColorBrush(aChatMessage.ChatMessagePreference.BackGroundColor);
+            Border.BorderBrush = new SolidColorBrush(aChatMessage.ChatMessagePreference.BorderColor);
+
+            LabelMessage.Text = aChatMessage.Message;
+            LabelTimeOfMessage.Content = DateTime.Now.ToString("hh:mm tt");
+        }
+    }
+}
